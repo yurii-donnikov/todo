91e2e625508fa4343ce75295b74969e6229908b1
Yurii <yurii@Yuriis-MacBook-Pro.local>
2026-03-15 18:24:44 +0100

    ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,30 @@ const express = require("express");
 const userRouter = require("./routes/user.routes");
 const taskRouter = require("./routes/task.routes");
 const cors = require("cors");
+const path = require("path");
 
 const pool = require("./db/db");
 
-const port = (process.env.port = 8080);
+const port = process.env.port || 8080;
 
 const app = express();
 app.use(
   cors({
     origin: "http://localhost:4200",
     credentials: true,
-  })
+  }),
 );
 
 app.use(express.json());
 
 app.use("/api", [userRouter, taskRouter]);
+
+const frontendPath = path.join(__dirname, "../frontend/dist/frontend/browser");
+app.use(express.static(frontendPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.get("/debug/db", async (req, res) => {
   const result = await pool.query("SELECT current_database()");
