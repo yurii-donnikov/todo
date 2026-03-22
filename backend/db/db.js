baffe44c4618b04e8b75966402c4f30547a8ff4b
Yurii <yurii@Yuriis-MacBook-Pro.local>
2026-03-22 12:12:59 +0100

    fixed db

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,23 @@
 const Pool = require("pg").Pool;
 
-const isProduction = process.env.NODE_ENV === "production";
+//const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? {
-        rejectUnauthorized: false,
-      }
-    : false,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  // ssl: isProduction
+  //   ? {
+  //       rejectUnauthorized: false,
+  //     }
+  //   : false,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
 });
 
 pool.on("connect", () => {
